ee04859005d3b1c0898cfd229eff824649423e98
goureesha <goureesha@users.noreply.github.com>
2026-03-03 06:09:03 +0000

    deploy: a38849942d2d6e872ac95cc3de1e0299ec43a2cc

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"assets/AssetManifest.json": "af38a6fa60749d030f4132c15e4b19f
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "index.html": "c2407aa10195f10ae326255938eb9282",
 "/": "c2407aa10195f10ae326255938eb9282",
-"flutter_bootstrap.js": "4df7b14f1ac2fd0ee202530efdc0f28f",
+"flutter_bootstrap.js": "f63e75a26fa3ddae8088002b2bf53f83",
 "main.dart.js": "4cb52f66888f1bc20b77aa0cbad4f37f",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
