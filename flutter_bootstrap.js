e1355cbc94aaab43218a5a94e8a1750d764e64a4
goureesha <goureesha@users.noreply.github.com>
2026-03-28 11:48:00 +0000

    deploy: 4b503d79aebc794e90b1d5e85af9a3f2d6b5b4f7

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3435814750"
+    serviceWorkerVersion: "3689779765"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,10 +33,10 @@ const RESOURCES = {"version.json": "0bf9d03ec8f107d6ab0977fb74bd18e9",
 "assets/AssetManifest.json": "7e84d408abb8b1d5b7f15797cd3225cd",
 "assets/assets/images/logo.png": "8b3be256f8c343bfe505d1de18b50462",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "0ea45b6976b85830a8207681974aaf40",
+"assets/fonts/MaterialIcons-Regular.otf": "8fdec6fff155e8f1affb0bc3cd6aac37",
 "assets/AssetManifest.bin.json": "40869625df6827315befec75cec0171e",
-"flutter_bootstrap.js": "f65f6bdf1e7151ee870b07e3058d800d",
-"main.dart.js": "54362e5ae3a9962941d1aa097df4c13a"};
+"flutter_bootstrap.js": "112967597bcfa1273a3a369aefb29e6d",
+"main.dart.js": "a77c16ebb3a6e167781614e50acf8e87"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
