46eaa80d98422180aa20376cc917b2ebd4af5390
goureesha <goureesha@users.noreply.github.com>
2026-04-18 02:40:11 +0000

    deploy: 989eff90243b22f73d5200a88c7f4618fce8667c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/assets/images/logo.png": "8b3be256f8c343bfe505d1de18b
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "privacy_policy.html": "10094331e2887570d5e079618ca2fdc8",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "d35ff10b019a7cb72c57bb8690d434f8",
+"flutter_bootstrap.js": "c305e5ba2ba4c6b2123cde9cec4827a7",
 "main.dart.js": "e04c7b23b8a591e37d055cb4fab5c4b8",
 "version.json": "40d383273be60e5774161ecd3d63e663",
 "googled477f94eebb558c3.html": "b609deddf10e215d559e8f07f04425df"};
