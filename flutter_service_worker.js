1c5e958f68a9de7ab32828c3394a64faf812341d
goureesha <goureesha@users.noreply.github.com>
2026-04-29 13:09:01 +0000

    deploy: a6b398c468552cb718f944dc466adb2709927037

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,7 +47,7 @@ const RESOURCES = {"assets/assets/fonts/NotoSansDevanagari-Bold.ttf": "c3ee3c730
 "canvaskit/skwasm_st.js": "d1326ceef381ad382ab492ba5d96f04d",
 "privacy_policy.html": "10094331e2887570d5e079618ca2fdc8",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "59f07cb360ae6681b264f2dc8b39ab4e",
+"flutter_bootstrap.js": "1f6a00501ed07c05f64960176ec8779e",
 "main.dart.js": "7177103eb6b3dea0eba5763421c4f8c8",
 "version.json": "ef8744097c0be2ab89d14c5640e8546a",
 "googled477f94eebb558c3.html": "b609deddf10e215d559e8f07f04425df"};
