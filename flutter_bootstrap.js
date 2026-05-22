3b9220847190f3868c709089bb162951c97d9fef
goureesha <goureesha@users.noreply.github.com>
2026-05-22 05:54:45 +0000

    deploy: fe29500700b206ada3fe7661ef7191f4e1ca19a9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "936730579"
+    serviceWorkerVersion: "3085488241"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -48,8 +48,8 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/chromium/canvaskit.js": "ba4a8ae1a65ff3ad81c6818fd47e348b",
 "canvaskit/canvaskit.js": "6cfe36b4647fbfa15683e09e7dd366bc",
 "canvaskit/skwasm.wasm": "f0dfd99007f989368db17c9abeed5a49",
-"flutter_bootstrap.js": "676357c6a89435fa87b612c62513de33",
-"main.dart.js": "3ca12a2828babc28227a1cd51249bc25",
+"flutter_bootstrap.js": "a59dbd8b5bf63905bda511b023310f8f",
+"main.dart.js": "43b59201ea176049968121d45626ba5f",
 "googled477f94eebb558c3.html": "b609deddf10e215d559e8f07f04425df"};
 // The application shell files that are downloaded before a service worker can
 // start.
