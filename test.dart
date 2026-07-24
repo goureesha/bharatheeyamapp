93b72a5d421e10c254c191da947875d7dccaade1
Bharatheeyam <bharatheeyam@app.com>
2026-07-24 11:00:57 +0530

    Remove ~150MB of junk: test files, logs, Python scripts, book pages, unused assets, build cache, old archives

--- test.dart
+++
@@ -1,4 +0,0 @@
-import 'package:sweph/sweph.dart';
-void main() {
-  print(Sweph.swe_houses(0, 0, 0, Hsys.P).runtimeType);
-}
--- test_compile.dart
+++
@@ -1,7 +0,0 @@
-import 'lib/core/calculator.dart';
-
-void main() {
-    print("Testing calculator parsing...");
-    final res = AstroCalculator.getPlanetDetail('???', 45.0, 1.0, 45.0);
-    print(res);
-}
