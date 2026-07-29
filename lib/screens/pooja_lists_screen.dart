3030f92a2c6ba36bdb49c051c87d601383302fae
Bharatheeyam <bharatheeyam@app.com>
2026-07-29 11:38:53 +0530

    Fix: add missing SharedPreferences import to settings_screen and pooja_lists_screen

--- lib/screens/pooja_lists_screen.dart
+++ lib/screens/pooja_lists_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/services.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../widgets/common.dart';
